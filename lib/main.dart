0f150720a82320039dbc9a8661d6e2ec864fcb1d
TOPWN <danggui@2dfire.com>
2018-09-16 12:44:56 +0800

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(
+      MyApp(),
+    );
 
 class MyApp extends StatelessWidget {
   @override
